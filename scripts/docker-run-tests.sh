248160cf3efb21adf46f95614d2241e220995150
Scott Smereka <scott.smereka@gmail.com>
2016-11-18 13:53:19 -0500

    Added db password to new bash env

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker run -d --name maids -e "DB_PASSWORD=$CASSANDRA_DB_PASSWORD" smartdevicelink/shaid-maids:$CIRCLE_TAG
-sudo lxc-attach -n "$(docker inspect --format "{{.Id}}" maids)" -- bash -c "cd /usr/src/app/ && npm run-script test-circleci"
+cmd="cd /usr/src/app/ && DB_PASSWORD=$CASSANDRA_DB_PASSWORD npm run-script test-circleci"
+sudo lxc-attach -n "$(docker inspect --format "{{.Id}}" maids)" -- bash -c "$cmd"
