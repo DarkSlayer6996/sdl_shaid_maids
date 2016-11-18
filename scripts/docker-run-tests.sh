94b70cb68fe30fb88fa99027da549552a7832b0e
Scott Smereka <scott.smereka@gmail.com>
2016-11-18 14:13:11 -0500

    Using docker net=host

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d --name maids -e "DB_PASSWORD=$CASSANDRA_DB_PASSWORD" smartdevicelink/shaid-maids:$CIRCLE_TAG
+docker run -d --name maids --net=host -e "DB_PASSWORD=$CASSANDRA_DB_PASSWORD" smartdevicelink/shaid-maids:$CIRCLE_TAG
 cmd="cd /usr/src/app/ && DB_PASSWORD=$CASSANDRA_DB_PASSWORD npm run-script test-circleci"
 sudo lxc-attach -n "$(docker inspect --format "{{.Id}}" maids)" -- bash -c "$cmd"
