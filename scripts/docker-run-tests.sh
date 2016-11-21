1e25a69ac632bdb69f5a100fd7894090184d4174
Scott Smereka <scott.smereka@gmail.com>
2016-11-21 11:19:51 -0500

    Setting ports to be more specific

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name maids --net=host -e "DB_PASSWORD=$CASSANDRA_DB_PASSWORD" smartdevicelink/shaid-maids:$CIRCLE_TAG
+docker run --name maids --net=host -e "DB_PASSWORD=$CASSANDRA_DB_PASSWORD" smartdevicelink/shaid-maids:123456789
