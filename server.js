34324cb15892db3759016bbf3e7fd711b37418f1
Scott Smereka <scott.smereka@gmail.com>
2016-12-20 13:39:46 -0500

    Added logging for start

--- server.js
+++ server.js
@@ -114,6 +114,7 @@ class Server {
       if(err) {
         self.log.error(err);
       } else {
+        self.log.info("Seneca listening with %s config.", NODE_ENV);
         self.emit('ready', self.seneca);
       }
     });
